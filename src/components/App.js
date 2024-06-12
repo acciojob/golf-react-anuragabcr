62c2c61c22959e8331cf817846a0c95eae17f9fc
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-06-12 05:09:05 +0000

    Golf

--- src/components/App.js
+++ src/components/App.js
@@ -7,26 +7,32 @@ class App extends Component {
         this.state = {
             renderBall: false,
             posi : 0,
-            ballPosition: { left: "0px" }
+            ballPosition: { left: 0 }
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
     };
 
     buttonClickHandler() {
-   
+        this.setState({renderBall: true})
    }
     renderBallOrButton() {
 		if (this.state.renderBall) {
 		    return <div className="ball" style={this.state.ballPosition}></div>
 		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
+		    return <button className="start" onClick={this.buttonClickHandler} >Start</button>
 		}
     }
 
     // bind ArrowRight keydown event
     componentDidMount() {
-      
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'ArrowRight') {
+                const newPosition = { ...this.state.ballPosition };
+                newPosition.left += 5; // Move ball 5 pixels right
+                this.setState({ ballPosition: newPosition });
+            }
+        });
     }
 
     render() {
