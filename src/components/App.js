9bb38933dcf61ed8cd4337892a17a3d849c861d6
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-06-12 05:20:03 +0000

    test 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isBallRendered: false,
-      ballPosition: { left: 0 },
+    constructor(props) {
+        super(props)
+        this.state = {
+            renderBall: false,
+            posi : 0,
+            ballPosition: { left: 5 }
+        };
+        this.renderChoice = this.renderBallOrButton.bind(this)
+        this.buttonClickHandler = this.buttonClickHandler.bind(this)
     };
-  }
 
-  componentDidMount() {
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'ArrowRight') {
-          const newPosition = { ...this.state.ballPosition };
-          newPosition.left += 5; // Move ball 5 pixels right
-          this.setState({ ballPosition: newPosition });
-        }
-      });
-  }
+    buttonClickHandler() {
+        this.setState({renderBall: true})
+   }
+    renderBallOrButton() {
+		if (this.state.renderBall) {
+		    return <div className="ball" style={this.state.ballPosition}></div>
+		} else {
+		    return <button className="start" onClick={this.buttonClickHandler} >Start</button>
+		}
+    }
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', (event) => {
-        if (event.key === 'ArrowRight') {
-          const newPosition = { ...this.state.ballPosition };
-          newPosition.left += 5; // Move ball 5 pixels right
-          this.setState({ ballPosition: newPosition });
-        }
-      });
-  }
+    // bind ArrowRight keydown event
+    componentDidMount() {
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'ArrowRight') {
+                const newPosition = { ...this.state.ballPosition };
+                newPosition.left += 5; // Move ball 5 pixels right
+                this.setState({ ballPosition: newPosition });
+            }
+        });
+    }
 
-  render() {
-    const { isBallRendered, ballPosition } = this.state;
-
-    return (
-      <div className="game-container">
-        {isBallRendered ? (
-          <div className="ball" style={{ left: `${ballPosition.left}px` }} />
-        ) : (
-          <button className="start" onClick={()=> {this.setState({ isBallRendered: true });}}>
-            Start Game
-          </button>
-        )}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="playground">
+                {this.renderBallOrButton()}
+            </div>
+        )
+    }
 }
+
 
 export default App;
